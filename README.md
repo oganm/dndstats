924c08aaded01d817b8783bb43c3ec62f10689dd
Ogan Mancarci <ogan.mancarci@gmail.com>
2018-08-18 00:37:04 -0700

    data release and wizardry

--- README.md
+++ README.md
@@ -12,3 +12,71 @@ See [here](https://github.com/oganm/dndstats/blob/master/docs/index.Rmd) for the
 The text of this document is licensed under [CC BY 4.0](https://creativecommons.org/licenses/by/4.0/) license
 
 The code blocks within the source code is licensed under [MIT license](https://opensource.org/licenses/MIT).
+
+
+## Data access
+
+This dataset is present in 2 forms: in its entirety that includes duplicates
+of characters and filtered version that only includes unique characters.
+
+Go [here](docs/charTable.tsv) for the complete data and [here](docs/uniqueTable.tsv) for the filtered one. Both have
+the same columns as explained below. The code to generate these tables can be found [here](https://github.com/oganm/dndstats/blob/master/dataProcess.R).
+
+Below are the descriptions of the columns in the files. If you think something you'd be interested
+in is missing, you can let me know.
+
+**name:** This column has hashes that represent character names. If the hashes are
+the same, that means the characters are the same. Real names are removed
+to protect character anonymity. Yes D&D characters have rights.
+
+**race:** This is the race field as it come out of the application. It is not really
+helpful as subrace and race information all mixed up together and unevenly available.
+It also includes some homebrew content. You probably want to use the **processedRace**
+column if you are interested in this.
+
+**background:** Background as it comes out of the application.
+
+**date:** Time & date of input. Dates before 2018-04-16 are unreliable as some has accidentally changed
+while moving files around.
+
+**class:** Class and level. Different classes are separated by `|` when needed.
+
+**justClass:** Class without level. Different classes are separated by `|` when needed.
+
+**subclass:** Subclasses. Again, separated by `|` when needed.
+
+**level:** Total character level.
+
+**feats:** Feats chosen by character. Separated by `|` when needed.
+
+**HP:** Character HP.
+
+**AC:** Character AC.
+
+**Str, Dex, Con, Int, Wis, Cha:** ability scores
+
+**alignment:** Alignment free text field. It is a mess, don't touch it. See **processedAlignment**,**good** and **lawful** instead.
+
+**skills:** List of skills with proficiency.  Separated by `|`.
+
+**weapons:** List weapons. Separated by `|`. It is somewhat of a mess as it allows free text inputs. See **processedWeapons**.
+
+**spells:** List of spells and their levels. Spells are separated by `|`s. Each spell has its level next to it
+separated by `*`s. This is a huge mess as its a free text field and some users included things like damage dice in them. See **processedSpells**.
+
+**day:** A shortened version of **date**. Only includes day information.
+
+**processedAlignment:** Processed version of the **alignment** column. Way people wrote up their alignments are manually sifted through and assigned to the matching aligmment. First character represents lawfulness (L, N, C), second one goodness (G,N,E). An empty string means alignment wasn't written or unclear.
+
+**good, lawful:** Isolated columns for goodness and lawfulness.
+
+**processedRace:** I have gone through the way **race** column is filled by the app and asigned them to correct
+races. If empty, indiciates a homebrew race not natively supported by the app.
+
+**processedSpells:** Formatting is same as the **spells** column but it is cleaned up.  Using string similarity I tried
+to match the spells to the full list of spells available in the official publications. The spell is removed if the spell I guessed does not have the correct level or doesn't include all words of the original spell and has too many modifications to be recognizable. It may have a few false matches but it should be mostly fine
+
+**processedWeapons:** Similar to **processedSpells**, **weapons** column is matched to the closest official weapon with some restrictions.
+
+**levelGroup:** splits levels into groups as used in the feat percentage plot. Only present in the filtered data
+but easy enough to make on your own.
--- dataProcess.R
+++ dataProcess.R
@@ -3,7 +3,13 @@ library(purrr)
 library(readr)
 library(glue)
 library(digest)
+library(dplyr)
 library(XML)
+library(ogbox) # github.com/oganm/ogbox
+library(wizaRd)
+library(stringr)
+
+
 # get all char files saved everywhere
 charFiles = c(list.files('/srv/shiny-server/printSheetApp/chars/',full.names = TRUE),
               list.files('/srv/shiny-server/interactiveSheet/chars/',full.names = TRUE),
@@ -60,23 +66,278 @@ charTable = chars %>% map(function(x){
                feats = x$feats[x$feats !=''] %>% paste(collapse = '|'),
                HP = x$currentHealth,
                AC = AC(x),
-               Str = x$abilityMods['Str'],
-               Dex = x$abilityMods['Dex'],
-               Con = x$abilityMods['Con'],
-               Int = x$abilityMods['Int'],
-               Wis = x$abilityMods['Wis'],
-               Cha = x$abilityMods['Cha'],
+               Str = x$abilityScores['Str'],
+               Dex = x$abilityScores['Dex'],
+               Con = x$abilityScores['Con'],
+               Int = x$abilityScores['Int'],
+               Wis = x$abilityScores['Wis'],
+               Cha = x$abilityScores['Cha'],
                alignment = x$Alignment,
                skills = x$skillProf %>% which %>% names %>% paste(collapse = '|'),
                weapons = x$weapons %>% map_chr('name') %>% gsub("\\|","",.)  %>% paste(collapse = '|'),
-               spells = glue('{x$spells$name %>% gsub("*|\\\\|","",.)}*{x$spells$level}') %>% glue::collapse('|') %>% {if(length(.)!=1){return('')}else{return(.)}},
-               day = x$date %>%  format('%m %d'),
+               spells = glue('{x$spells$name %>% gsub("\\\\*|\\\\|","",.)}*{x$spells$level}') %>% glue::collapse('|') %>% {if(length(.)!=1){return('')}else{return(.)}},
+               day = x$date %>%  format('%m %d %y'),
                stringsAsFactors = FALSE)
 }) %>% do.call(rbind,.)
 
 
 
+# post processing -----
+# the way races are encoded in the app is a little silly. sub-races are 
+# not recorded separately. essentially race information is lost other
+# than a text field after it's effects are applied during creation.
+# The text field is also not too consistent. For instance if you are a 
+# variant it'll simply say "Variant" but if you are a variant human
+# it'll only say human
+# here, I define regex that matches races.
+# kind of an overkill as only few races actually required special care
+races = c(Aarakocra = 'Aarakocra',
+          Aasimar = 'Aasimar',
+          Bugbear= 'Bugbear',
+          Dragonborn = 'Dragonborn',
+          Dwarf = 'Dwarf',
+          Elf = '(?<!Half-)Elf',
+          Firbolg = 'Firbolg',
+          Genasi= 'Genasi',
+          Gith = 'Geth',
+          Gnome = 'Gnome',
+          Goblin='Goblin',
+          Goliath = 'Goliath',
+          'Half-Elf' = '(Half-Elf)|(^Variant)',
+          'Half-Orc' = 'Half-Orc',
+          Halfling = 'Halfling',
+          Hobgoblin = 'Hobgoblin',
+          Human = 'Human',
+          Kenku = 'Kenku',
+          Kobold = 'Kobold',
+          Lizardfolk = 'Lizardfolk',
+          Orc = '(?<!Half-)Orc',
+          'Yaun-Ti' = 'Serpentblood',
+          Tabaxi = 'Tabaxi',
+          Tiefling ='Tiefling|Lineage',
+          Triton = 'Triton',
+          Turtle = 'Turtle|Tortle')
 
+align = list(NG = c('NG',
+                    '"Good"',
+                    "Neuteral Good",
+                    "Neutral Good",
+                    "Nuetral Goodt",
+                    "Neutral/Good",
+                    "Neutral good",
+                    'neutral good',
+                    'neutral-good',
+                    'Neutral Good ',
+                    'Nuetral Good',
+                    'N/G'),
+             CG = c('Chaotic Good',
+                    'CG',
+                    'Chacotic Good',
+                    'Chaotic good',
+                    'Good Chaotic',
+                    'chaotic good',
+                    'Chaotic Good '),
+             LG = c('Lawful Good',
+                    'Lawful Good ',
+                    'L-G',
+                    'LG',
+                    'lawful good',
+                    'Lawful good'),
+             NN = c('Neutral',
+                    'neutral ',
+                    'Neutral ',
+                    'n',
+                    'N',
+                    'True Neutral',
+                    'True Neutral ',
+                    'neutral',
+                    'TN',
+                    'Neutral Neutral',
+                    'true neutral',
+                    'Neutral neutral'),
+             CN = c('Chaotic Neutral',
+                    'CN',
+                    'chaotic neutral',
+                    'Chaotic neutral',
+                    'chaotic nuetral',
+                    'Chaotic Nuetral',
+                    'cn',
+                    'Chaotic Neutral ',
+                    'neutral chaotic ',
+                    'neutral chaotic'),
+             LN = c('Lawful Neutral',
+                    'lawful neutral ',
+                    'Leal e Neutro',
+                    'lawful - neutral',
+                    'LN',
+                    'Lawful Neutral (good-ish)',
+                    'lawful neutral',
+                    'lawful neutral'),
+             NE = c('Neutral Evil'),
+             LE = c('Lawful Evil','LE'),
+             CE = c('CE','Chaotic Evil'))
+
+goodEvil = list(`E` = c('NE','LE','CE'),
+                `N` = c('LN','CN','NN'),
+                `G` = c('NG','LG','CG'))
+
+lawfulChaotic = list(`C` = c('CN','CG','CE'),
+                     `N` = c('NG','NE','NN'),
+                     `L` = c('LG','LE','LN'))
+
+# lists any alignment text I'm not processing
+charTable$alignment  %>% {.[!. %in% unlist(align)]} %>% table %>% sort %>% names
+
+checkAlignment = function(x,legend){
+    x = names(legend)[findInList(x,legend)]
+    if(length(x) == 0){
+        return('')
+    } else{
+        return(x)
+    }
+}
+
+
+charTable %<>% mutate(processedAlignment = alignment %>% purrr::map_chr(checkAlignment,align),
+                        good = processedAlignment %>% purrr::map_chr(checkAlignment,goodEvil) %>% 
+                            factor(levels = c('E','N','G')),
+                        lawful = processedAlignment %>% 
+                            purrr::map_chr(checkAlignment,lawfulChaotic) %>% factor(levels = c('C','N','L'))) 
+
+charTable %<>% mutate(processedRace = race %>% sapply(function(x){
+    out = races %>% sapply(function(y){
+        grepl(pattern = y, x,perl = TRUE,ignore.case = TRUE)
+    }) %>% which %>% names
+    
+    if(length(out) == 0 | length(out)>1){
+        out = ''
+    }
+    
+    return(out)
+}))
+
+# remove personal info
+
+shortestDigest = function(vector){
+    digested  = vector %>% map_chr(digest,'sha1')
+    uniqueDigested =  digested %>% unique 
+    
+    collusionLimit = 1:40 %>% sapply(function(i){
+        substr(uniqueDigested,40-i,40)%>% unique %>% length
+    }) %>% which.max %>% {.+1}
+    
+    digested %<>%  substr(40-collusionLimit,40)
+}
+
+
+charTable$name %<>% shortestDigest
+charTable$ip %<>% shortestDigest
+charTable$finger %<>% shortestDigest
+charTable$hash %<>% shortestDigest
+
+spells = wizaRd::spells
+
+spells = c(spells, list('.' = list(level = as.integer(99))))
+class(spells) = 'list'
+
+legitSpells =spells %>% names
+
+
+processedSpells = charTable$spells %>% sapply(function(x){
+    if(x==''){
+        return('')
+    }
+    spellNames = x %>% str_split('\\|') %>% {.[[1]]} %>% str_split('\\*') %>% map_chr(1)
+    spellLevels =  x %>% str_split('\\|') %>% {.[[1]]} %>% str_split('\\*') %>% map_chr(2)
+    
+    distanceMatrix = adist(tolower(spellNames), tolower(legitSpells),costs = list(ins=2, del=2, sub=3), counts = TRUE)
+    
+    rownames(distanceMatrix) = spellNames
+    colnames(distanceMatrix) = legitSpells
+    
+    predictedSpell = distanceMatrix %>% apply(1,which.min) %>% {legitSpells[.]}
+    distanceScores =  distanceMatrix %>% apply(1,min) 
+    predictedSpellLevel = spells[predictedSpell] %>% purrr::map_int('level')
+    
+    ins = attributes(distanceMatrix)$counts[,distanceMatrix %>% apply(1,which.min),'ins'] %>% as.matrix  %>% diag
+    del = attributes(distanceMatrix)$counts[,distanceMatrix %>% apply(1,which.min),'del'] %>% as.matrix %>% diag
+    sub = attributes(distanceMatrix)$counts[,distanceMatrix %>% apply(1,which.min),'sub'] %>% as.matrix %>% diag
+    isItIn = predictedSpell %>% str_split(' |/') %>% map(function(x){
+        x[!x %in% c('and','or','of','to','the')]
+    }) %>% 
+    {sapply(1:length(.),function(i){
+        all(sapply(.[[i]],grepl,x =spellNames[i],ignore.case=TRUE))
+    })}
+    
+    spellFrame = data.frame(spellNames,predictedSpell,spellLevels,predictedSpellLevel,distanceScores,ins,del,sub,isItIn,stringsAsFactors = FALSE)
+    
+    spellFrame %<>% filter(as.integer(spellLevels)==predictedSpellLevel &( isItIn | (sub < 5 & del < 5 & ins < 5)))
+    
+    paste0(spellFrame$predictedSpell,'*',spellFrame$predictedSpellLevel,collapse ='|')
+})
+charTable$processedSpells = processedSpells
+
+# x = 1:nrow(charTable) %>% sapply(function(i){adist(charTable$spells[i],charTable$processedSpells[i])}) %>% {.>20} %>% {charTable$spells[.]} %>% {.[43]}
+# x = 1:nrow(charTable) %>% sapply(function(i){adist(charTable$spells[i],charTable$processedSpells[i])}) %>% {.>20} %>% {charTable$spells[.]} %>% {.[70]}
+# x = 1:nrow(charTable) %>% sapply(function(i){adist(charTable$spells[i],charTable$processedSpells[i])}) %>% {.>20} %>% {charTable$spells[.]} %>% {.[88]}
+
+# download.file('https://www.dropbox.com/s/4f7zdx09nkfa9as/Core.xml?dl=1',destfile = 'Core.xml')
+# allRules = xmlParse('Core.xml') %>% xmlToList()
+# fightClubItems = allRules[names(allRules) == 'item']
+# saveRDS(fightClubItems,'fightClubItems.rds')
+
+# fightClubItems =  readRDS('fightClubItems.rds')
+# names(fightClubItems) = allRules %>% map('name') %>% as.character
+# 
+# fightClubItems %>% map_chr('type') %>% {. %in% 'M'} %>% {fightClubItems[.]} %>% map_chr('name')
+# fightClubItems %>% map_chr('type') %>% {. %in% 'R'} %>% {fightClubItems[.]} %>% map_chr('name')
+
+legitWeapons = c(# fightClubItems %>% map_chr('type') %>% {. %in% 'M'} %>% {fightClubItems[.]} %>% map_chr('name'),
+                 # fightClubItems %>% map_chr('type') %>% {. %in% 'R'} %>% {fightClubItems[.]} %>% map_chr('name'),
+                 'Crossbow, Light', 'Dart', 'Shortbow', 'Sling',
+                 'Blowgun', 'Crossbow, hand', 'Crossbow, Heavy', 'Longbow', 'Net',
+                 'Club','Dagger','Greatclub','Handaxe','Javelin','Light hammer','Mace','Quarterstaff','Sickle','Spear','Unarmed Strike',
+                 'Battleaxe','Flail','Glaive','Greataxe','Greatsword','Halberd','Lance','Longsword','Maul','Morningstar','Pike','Rapier','Scimitar','Shortsword','Trident','War pick','Warhammer','Whip')
+
+processedWeapons = charTable$weapons %>% sapply(function(x){
+    if(x==''){
+        return('')
+    }
+    weaponNames = x %>% str_split('\\|') %>% {.[[1]]} 
+
+    distanceMatrix = adist(tolower(weaponNames), tolower(legitWeapons),costs = list(ins=2, del=2, sub=3), counts = TRUE)
+    
+    rownames(distanceMatrix) = weaponNames
+    colnames(distanceMatrix) = legitWeapons
+    
+    predictedWeapon = distanceMatrix %>% apply(1,which.min) %>% {legitWeapons[.]}
+    distanceScores =  distanceMatrix %>% apply(1,min) 
+    
+    ins = attributes(distanceMatrix)$counts[,distanceMatrix %>% apply(1,which.min),'ins'] %>% as.matrix  %>% diag
+    del = attributes(distanceMatrix)$counts[,distanceMatrix %>% apply(1,which.min),'del'] %>% as.matrix %>% diag
+    sub = attributes(distanceMatrix)$counts[,distanceMatrix %>% apply(1,which.min),'sub'] %>% as.matrix %>% diag
+    isItIn = predictedWeapon %>% str_split(' |/') %>% map(function(x){
+        x[!x %in% c('and','or','of','to','the')]
+    }) %>% 
+    {sapply(1:length(.),function(i){
+        all(sapply(.[[i]],grepl,x =weaponNames[i],ignore.case=TRUE))
+    })}
+    
+    weaponFrame = data.frame(weaponNames,predictedWeapon,distanceScores,ins,del,sub,isItIn,stringsAsFactors = FALSE)
+    
+    weaponFrame %<>% filter(isItIn|  (sub < 2 & del < 2 & ins < 2))
+    
+    paste0(weaponFrame$predictedWeapon %>% unique,collapse ='|')
+})
+
+charTable$processedWeapons = processedWeapons
+
+# x = 1:nrow(charTable) %>% sapply(function(i){adist(charTable$weapons[i],charTable$processedWeapons[i])}) %>% {.>20} %>% {charTable$weapons[.]} %>% {.[10]}
+
+    
+unsecureFields = c('ip','finger','hash')
+
+charTable = charTable[!names(charTable) %in% unsecureFields]
 
 # user id ------
 # userID = c()
